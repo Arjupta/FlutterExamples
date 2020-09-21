9929a34dd019b88b0620d50b448e1395bdedb507
ARJUPTA <64064110+ARJUPTA@users.noreply.github.com>
2020-09-21 23:53:33 +0530

    Using OkHttp as http and decoding Json Data

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import '../services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -17,8 +19,19 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    Response response = await get('');
-    print(response.statusCode);
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02');
+    String data = response.body;
+    // var longitude = jsonDecode(data)['coord']['lon'];
+    // print(longitude);
+    // var weatherDescription = jsonDecode(data)['weather'][0]['main'];
+    // print(weatherDescription);
+    var decoded_data = jsonDecode(data);
+    double temperature = decoded_data['main']['temp'];
+    int condition = decoded_data['weather'][0]['id'];
+    String cityName = decoded_data['name'];
+
+    print('${temperature}_${condition}_$cityName');
   }
 
   @override
