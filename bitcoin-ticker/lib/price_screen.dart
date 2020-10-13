9f8e9615d6b9d3db4245851ea28fbbb0940d1184
ARJUPTA <64064110+ARJUPTA@users.noreply.github.com>
2020-10-13 23:50:50 +0530

    Working DropdownButton with Hardcoded data.

--- bitcoin-ticker/lib/price_screen.dart
+++ bitcoin-ticker/lib/price_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'coin_data.dart';
+
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = "USD";
+  List<String> list = currenciesList;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +46,17 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton(
+              value: selectedCurrency,
+              items:
+                  list.map((e) => DropdownMenuItem(child: Text(e), value: e)),
+              onChanged: (val) {
+                print(val);
+                setState(() {
+                  selectedCurrency = val;
+                });
+              },
+            ),
           ),
         ],
       ),
