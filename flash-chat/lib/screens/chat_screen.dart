546a5641dc1ec3a03455f811181f16ec7c3f0848
ARJUPTA <64064110+ARJUPTA@users.noreply.github.com>
2020-11-02 22:15:39 +0530

    Flash_Chat Completed
    This is fully functional now .We will incorporate auto-login for already
    signed in users in future.

--- flash-chat/lib/screens/chat_screen.dart
+++ flash-chat/lib/screens/chat_screen.dart
@@ -3,6 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 
+var _firestore = FirebaseFirestore.instance;
+User loggedInUser;
+
 class ChatScreen extends StatefulWidget {
   static const String id = "chat_screen";
   @override
@@ -11,8 +14,7 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
-  final _firestore = FirebaseFirestore.instance;
-  User loggedInUser;
+  final msgTextController = TextEditingController();
   String messageText = "";
 
   void getCurrentUser() async {
@@ -27,20 +29,20 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  void getMessages() async {
-    final messages = await _firestore.collection('messages').getDocuments();
-    for (var message in messages.documents) {
-      print(message.data);
-    }
-  }
+  // void getMessages() async {
+  //   final messages = await _firestore.collection('messages').getDocuments();
+  //   for (var message in messages.documents) {
+  //     print(message.data);
+  //   }
+  // }
 
-  void messagesStream() async {
-    await for (var snapshot in _firestore.collection('messages').snapshots()) {
-      for (var message in snapshot.docs) {
-        print(message.data());
-      }
-    }
-  }
+  // void messagesStream() async {
+  //   await for (var snapshot in _firestore.collection('messages').snapshots()) {
+  //     for (var message in snapshot.docs) {
+  //       print(message.data());
+  //     }
+  //   }
+  // }
 
   @override
   void initState() {
@@ -69,24 +71,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            StreamBuilder<QuerySnapshot>(
-              stream: _firestore.collection('messages').snapshots(),
-              builder: (context, snapshot) {
-                List<Text> widgets = [];
-                if (!snapshot.hasData) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                final messages = snapshot.data.docs;
-                for (var message in messages) {
-                  print(message.data());
-                  final messageText = message.data()['text'];
-                  final mesageSender = message.data()['sender'];
-                  final messageWidget = Text('$messageText from $mesageSender');
-                  widgets.add(messageWidget);
-                }
-                return Column(children: widgets);
-              },
-            ),
+            MyMessagesStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -94,6 +79,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: msgTextController,
                       onChanged: (value) {
                         messageText = value;
                       },
@@ -102,11 +88,13 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   FlatButton(
                     onPressed: () {
-                      Map<String, String> data = {
+                      Map<String, dynamic> data = {
                         'sender': loggedInUser.email,
-                        'text': messageText
+                        'text': messageText,
+                        'time': DateTime.now().toUtc().millisecondsSinceEpoch,
                       };
-                      _firestore.collection('messages').add(data);
+                      _firestore.collection('Messages').add(data);
+                      msgTextController.clear();
                     },
                     child: Text(
                       'Send',
@@ -118,6 +106,86 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MyMessagesStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('Messages').orderBy('time').snapshots(),
+      builder: (context, snapshot) {
+        List<Widget> widgets = [];
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+        final messages = snapshot.data.docs.reversed;
+        for (var message in messages) {
+          print(message.data());
+          final messageText = message.data()['text'];
+          final mesageSender = message.data()['sender'];
+          final currentUser = loggedInUser.email;
+          widgets.add(
+            MessageBubble(
+              messageText: messageText,
+              messageSender: mesageSender,
+              isMe: currentUser == mesageSender,
+            ),
+          );
+        }
+        // return Column(children: widgets);
+        return Expanded(
+          child: ListView(
+            reverse: true,
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            children: widgets,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble({this.messageText, this.messageSender, this.isMe});
+  final String messageText;
+  final String messageSender;
+  final bool isMe;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: Column(
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            '$messageSender',
+            style: TextStyle(fontSize: 12, color: Colors.black54),
+          ),
+          Material(
+            borderRadius: BorderRadius.only(
+              topRight: isMe ? Radius.circular(0) : Radius.circular(30),
+              topLeft: isMe ? Radius.circular(30) : Radius.circular(0),
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30),
+            ),
+            elevation: 5,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                '$messageText',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: isMe ? Colors.white : Colors.black54,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
