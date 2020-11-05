dbcb152484bb17560ba3fa4f60778f3de0c81e2f
ARJUPTA <64064110+ARJUPTA@users.noreply.github.com>
2020-11-05 23:53:05 +0530

    Initial stub for Todoey

---
+++ todoey/lib/screens/TasksScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TaskScreen extends StatefulWidget {
+  @override
+  _TaskScreenState createState() => _TaskScreenState();
+}
+
+class _TaskScreenState extends State<TaskScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
